579d261e062c80eb4abebd05e3b02e64fac68fac
Mykyta Klymenko <mikita.klymenko153@gmail.com>
2024-10-31 14:37:45 +0200

    change website name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,9 @@ import "./globals.css";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Офіційна сторінка BEST CTF 2024",
-  description: "BEST CTF - це командні змагання з кібербезпеки,  в яких учасники виконують завдання з різних категорій. Команди повинні знайти 'прапорці' і заробити за них бали.",
+  title: "BEST CTF 2024",
+  description:
+    "BEST CTF - це командні змагання з кібербезпеки,  в яких учасники виконують завдання з різних категорій. Команди повинні знайти 'прапорці' і заробити за них бали.",
 };
 
 export default async function RootLayout({
